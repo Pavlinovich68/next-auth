52cb98c1e5879ca2cfc7abd3c97ce7f9b135a0a5
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2023-09-29 03:59:10 +0500

    3:25:42

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import {PrismaClient} from "@prisma/client";
+
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
     providers: [
@@ -7,6 +12,34 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            async authorize(credentials){
+                try {
+                    const user = await prisma.users.findFirst({
+                        where: {
+                            email: credentials.email
+                        }
+                    });
+                    if (user) {
+                        const isPasswordCorrect = await bcrypt.compare(
+                            credentials.password, user.password
+                        );
+                        if (isPasswordCorrect) {
+                            return user;
+                        } else {
+                            throw new Error("Wrong Credentials");
+                        }
+                    } else {
+                        throw new Error("User not found");
+                    }
+                } catch (err) {
+                    throw new Error(err);
+                }
+
+            }
+        })
     ],
 });
 
