a9df9467924979b37df7c1a212c4fc80766363b0
s.pavlov@mininform74.ru <s.pavlov@mininform74.ru>
2023-09-29 15:49:00 +0500

    3:40:49

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,6 +2,9 @@ import postService from "./service"
 import { NextResponse,  } from "next/server";
 
 export const GET = async (request) => {
+    const url = new URL(request.url);
+    const username = url.searchParams.get("username");
+
     try {
         const result = await postService.read();
         let json_response = {
